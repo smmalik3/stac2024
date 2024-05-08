358d4a20e27b2472668ca9791884db9bfb8ff5a7
Malik, Salman <salmalik@deloitte.com>
2024-05-08 11:48:16 -0400

    updated terraform code and added lambda

--- main.tf
+++ main.tf
@@ -11,59 +11,69 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket         = "terraformstatestorage-stac2023-1"
+    bucket         = "terraformstatestorage-stac2024-1"
     key            = "terraformstate/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraformstatedb-stac2023-1"
+    dynamodb_table = "terraformstatedb-stac2024-1"
   }
 }
 
-resource "aws_api_gateway_rest_api" "salesforce" {
-  name        = "salesforce-api"
-  description = "Salesforce API"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "terraformstatestorage-stac2024-1"
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = {
+    stac2024=true
+  }
 }
 
-resource "aws_api_gateway_resource" "salesforce" {
-  rest_api_id = aws_api_gateway_rest_api.salesforce.id
-  parent_id   = aws_api_gateway_rest_api.salesforce.root_resource_id
+resource "aws_api_gateway_rest_api" "salesforce-2024" {
+  name        = "salesforce-api-2024"
+  description = "Salesforce API-2024"
+}
+
+resource "aws_api_gateway_resource" "salesforce-2024" {
+  rest_api_id = aws_api_gateway_rest_api.salesforce-2024.id
+  parent_id   = aws_api_gateway_rest_api.salesforce-2024.root_resource_id
   path_part   = "file-upload"
 }
 
-resource "aws_api_gateway_method" "salesforce" {
-  rest_api_id   = aws_api_gateway_rest_api.salesforce.id
-  resource_id   = aws_api_gateway_resource.salesforce.id
+resource "aws_api_gateway_method" "salesforce-2024" {
+  rest_api_id   = aws_api_gateway_rest_api.salesforce-2024.id
+  resource_id   = aws_api_gateway_resource.salesforce-2024.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_lambda_permission" "salesforce" {
+resource "aws_lambda_permission" "salesforce-2024" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fileReceived.arn
+  function_name = aws_lambda_function.getFileFromSalesforce.arn
   principal     = "apigateway.amazonaws.com"
 }
 
-resource "aws_api_gateway_integration" "salesforce" {
-  rest_api_id             = aws_api_gateway_rest_api.salesforce.id
-  resource_id             = aws_api_gateway_resource.salesforce.id
-  http_method             = aws_api_gateway_method.salesforce.http_method
+resource "aws_api_gateway_integration" "salesforce-2024" {
+  rest_api_id             = aws_api_gateway_rest_api.salesforce-2024.id
+  resource_id             = aws_api_gateway_resource.salesforce-2024.id
+  http_method             = aws_api_gateway_method.salesforce-2024.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.fileReceived.invoke_arn
+  uri                     = aws_lambda_function.getFileFromSalesforce.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "salesforce" {
-  depends_on  = [aws_api_gateway_integration.salesforce]
-  rest_api_id = aws_api_gateway_rest_api.salesforce.id
+resource "aws_api_gateway_deployment" "salesforce-2024" {
+  depends_on  = [aws_api_gateway_integration.salesforce-2024]
+  rest_api_id = aws_api_gateway_rest_api.salesforce-2024.id
   stage_name  = "prod"
   variables = {
-    "lambdaAlias" = aws_lambda_alias.salesforce.name
+    "lambdaAlias" = aws_lambda_alias.salesforce-2024.name
   }
 }
 
-resource "aws_lambda_alias" "salesforce" {
+resource "aws_lambda_alias" "salesforce-2024" {
   name             = "prod"
-  function_name    = aws_lambda_function.fileReceived.function_name
+  function_name    = aws_lambda_function.getFileFromSalesforce.function_name
   function_version = "$LATEST"
 }
 
@@ -147,302 +157,36 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_s3_bucket" "resumeuploads5" {
-  bucket = "resumeuploads5"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_textract_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonTextractFullAccess"
-  role       = aws_iam_role.lambda_role.name
-}
-
-data "aws_iam_policy_document" "textract_lambda_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "textract:DetectDocumentText"
-    ]
-    resources = ["*"]
+resource "aws_lambda_function" "getFileFromSalesforce" {
+  filename         = "getFileFromSalesforce.zip"
+  function_name    = "getFileFromSalesforce"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "getFileFromSalesforce/handler.getFile"
+  source_code_hash = filebase64sha256("getFileFromSalesforce.zip")
+  runtime          = "nodejs14.x"
+  timeout          = var.LAMBDA_TIMEOUT
+  environment {
+    variables = {
+      BUCKET_NAME       = aws_s3_bucket.stac2024-saved-files.id
+      LAMBDA_TIMEOUT    = var.LAMBDA_TIMEOUT
+    }
   }
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs_policy
+  ]
 }
 
-resource "aws_iam_policy" "textract_lambda_policy" {
-  name   = "textract-lambda-policy"
-  policy = data.aws_iam_policy_document.textract_lambda_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "textract_lambda_attachment" {
-  policy_arn = aws_iam_policy.textract_lambda_policy.id
-  role       = aws_iam_role.lambda_role.id
-}
-
-resource "aws_iam_role_policy_attachment" "s3_fullaccess_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  role       = aws_iam_role.lambda_role.name
+resource "aws_s3_bucket" "stac2024-saved-files" {
+  bucket = "stac2024-saved-files"
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = {
+    stac2024=true
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_role.name
-}
-
-resource "aws_lambda_function" "fileUploaded1" {
-  filename         = "fileUploaded1.zip"
-  function_name    = "fileUploaded1"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "fileUploaded1/handler.readS3File"
-  source_code_hash = filebase64sha256("fileUploaded1.zip")
-  runtime          = "nodejs14.x"
-  timeout          = var.LAMBDA_TIMEOUT  // Update the timeout value in seconds
-  environment {
-    variables = {
-      BUCKET_NAME       = aws_s3_bucket.resumeuploads5.id
-      OPENAI_API_KEY    = var.OPENAI_API_KEY
-      CLIENT_ID         = var.CLIENT_ID
-      CLIENT_SECRET     = var.CLIENT_SECRET
-      SF_USERNAME       = var.SF_USERNAME
-      SF_PASSWORD       = var.SF_PASSWORD
-      SF_SECURITY_TOKEN = var.SF_SECURITY_TOKEN
-      LAMBDA_TIMEOUT    = var.LAMBDA_TIMEOUT
-    }
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_logs_policy,
-    aws_iam_role_policy_attachment.lambda_textract_policy
-  ]
-}
-
-resource "aws_lambda_function" "fileReceived" {
-  filename         = "fileReceived.zip"
-  function_name    = "fileReceived"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "fileReceived/handler.getFile"
-  source_code_hash = filebase64sha256("fileReceived.zip")
-  runtime          = "nodejs14.x"
-  timeout          = var.LAMBDA_TIMEOUT
-  environment {
-    variables = {
-      BUCKET_NAME       = aws_s3_bucket.resumeuploads5.id
-      LAMBDA_TIMEOUT    = var.LAMBDA_TIMEOUT
-    }
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_logs_policy
-  ]
-}
-
-resource "aws_cloudwatch_log_group" "fileUploaded1_logs" {
-  name              = "/aws/lambda/fileUploaded1"
-  retention_in_days = 14
-}
-
-resource "aws_cloudwatch_log_group" "fileReceived" {
-  name              = "/aws/lambda/fileReceived"
-  retention_in_days = 14
-}
-
-data "aws_iam_policy_document" "lambda_logging" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-
-    resources = ["arn:aws:logs:*:*:*"]
-  }
-}
-
-resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda_logging"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
-  policy      = data.aws_iam_policy_document.lambda_logging.json
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.resumeuploads5.bucket
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.fileUploaded1.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-}
-
-resource "aws_lambda_permission" "s3_permission" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fileUploaded1.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.resumeuploads5.arn
-}
-
-resource "aws_lambda_permission" "allow_textract_invoke" {
-  statement_id    = "AllowTextractInvoke"
-  action          = "lambda:InvokeFunction"
-  function_name   = aws_lambda_function.fileUploaded1.arn
-  principal       = "textract.amazonaws.com"
-  source_account  = "690711176673"  # Update with your AWS account ID
-  source_arn      = "arn:aws:textract:us-east-1:690711176673:document-understanding-pipeline/*"
-}
-
-##
-##ServiceNow Configuration TODO: refactor into separate modules for terraform
-##
-
-resource "aws_api_gateway_rest_api" "servicenow" {
-  name        = "servicenow-api"
-  description = "ServiceNow API"
-}
-
-resource "aws_api_gateway_resource" "servicenow" {
-  rest_api_id = aws_api_gateway_rest_api.servicenow.id
-  parent_id   = aws_api_gateway_rest_api.servicenow.root_resource_id
-  path_part   = "file-upload"
-}
-
-resource "aws_api_gateway_method" "servicenow" {
-  rest_api_id   = aws_api_gateway_rest_api.servicenow.id
-  resource_id   = aws_api_gateway_resource.servicenow.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_lambda_permission" "servicenow" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.receiveFromServiceNow.arn
-  principal     = "apigateway.amazonaws.com"
-}
-
-resource "aws_api_gateway_integration" "servicenow" {
-  rest_api_id             = aws_api_gateway_rest_api.servicenow.id
-  resource_id             = aws_api_gateway_resource.servicenow.id
-  http_method             = aws_api_gateway_method.servicenow.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.receiveFromServiceNow.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "servicenow" {
-  depends_on  = [aws_api_gateway_integration.servicenow]
-  rest_api_id = aws_api_gateway_rest_api.servicenow.id
-  stage_name  = "prod"
-  variables = {
-    "lambdaAlias" = aws_lambda_alias.servicenow.name
-  }
-}
-
-resource "aws_lambda_alias" "servicenow" {
-  name             = "prod"
-  function_name    = aws_lambda_function.receiveFromServiceNow.function_name
-  function_version = "$LATEST"
-}
-
-resource "aws_s3_bucket" "resumeuploadsservicenow1" {
-  bucket = "resumeuploadsservicenow1"
-}
-
-resource "aws_lambda_function" "sendToServiceNow" {
-  filename         = "sendToServiceNow.zip"
-  function_name    = "sendToServiceNow"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "sendToServiceNow/handler.sendToServiceNow"
-  source_code_hash = filebase64sha256("sendToServiceNow.zip")
-  runtime          = "nodejs14.x"
-  timeout          = var.LAMBDA_TIMEOUT  // Update the timeout value in seconds
-  environment {
-    variables = {
-      BUCKET_NAME       = aws_s3_bucket.resumeuploadsservicenow1.id
-      OPENAI_API_KEY    = var.OPENAI_API_KEY
-      CLIENT_ID         = var.CLIENT_ID
-      CLIENT_SECRET     = var.CLIENT_SECRET
-      SN_USERNAME       = var.SN_USERNAME
-      SN_PASSWORD       = var.SN_PASSWORD
-      LAMBDA_TIMEOUT    = var.LAMBDA_TIMEOUT
-      SN_PATCH_URL      = var.SN_PATCH_URL
-      OPENAI_ORG_ID     = var.OPENAI_ORG_ID
-    }
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_logs_policy,
-    aws_iam_role_policy_attachment.lambda_textract_policy
-  ]
-}
-
-resource "aws_lambda_function" "receiveFromServiceNow" {
-  filename         = "receiveFromServiceNow.zip"
-  function_name    = "receiveFromServiceNow"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "receiveFromServiceNow/handler.receiveFromServiceNow"
-  source_code_hash = filebase64sha256("receiveFromServiceNow.zip")
-  runtime          = "nodejs14.x"
-  timeout          = var.LAMBDA_TIMEOUT
-  environment {
-    variables = {
-      BUCKET_NAME       = aws_s3_bucket.resumeuploadsservicenow1.id
-      LAMBDA_TIMEOUT    = var.LAMBDA_TIMEOUT
-    }
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_logs_policy
-  ]
-}
-
-resource "aws_cloudwatch_log_group" "sendToServiceNow_logs" {
-  name              = "/aws/lambda/sendToServiceNow"
-  retention_in_days = 14
-}
-
-resource "aws_cloudwatch_log_group" "receiveFromServiceNow" {
-  name              = "/aws/lambda/receiveFromServiceNow"
-  retention_in_days = 14
-}
-
-data "aws_iam_policy_document" "lambda_logging_servicenow" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-
-    resources = ["arn:aws:logs:*:*:*"]
-  }
-}
-
-resource "aws_iam_policy" "lambda_logging_servicenow" {
-  name        = "lambda_logging_servicenow"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
-  policy      = data.aws_iam_policy_document.lambda_logging_servicenow.json
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification_servicenow" {
-  bucket = aws_s3_bucket.resumeuploadsservicenow1.bucket
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.sendToServiceNow.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-}
-resource "aws_lambda_permission" "s3_permission_servicenow" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.sendToServiceNow.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.resumeuploadsservicenow1.arn
-}
-
-resource "aws_lambda_permission" "allow_textract_invoke_servicenow" {
-  statement_id    = "AllowTextractInvoke"
-  action          = "lambda:InvokeFunction"
-  function_name   = aws_lambda_function.sendToServiceNow.arn
-  principal       = "textract.amazonaws.com"
-  source_account  = "690711176673"  # Update with your AWS account ID
-  source_arn      = "arn:aws:textract:us-east-1:690711176673:document-understanding-pipeline/*"
 }
